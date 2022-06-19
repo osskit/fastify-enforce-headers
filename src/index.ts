2ec12a33bd274e7c9b08ab6f2310c186fc8a9176
Sharon Grossman <sharong1337@gmail.com>
2022-06-19 16:03:53 +0300

    initial commits

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,24 @@
-import type { FastifyInstance, FastifyPluginCallback, FastifyError } from 'fastify';
-import fp from 'fastify-plugin'
+import type { FastifyInstance, FastifyPluginAsync } from 'fastify';
+import fp from 'fastify-plugin';
 import createError from 'http-errors';
 
 interface FastifyEnforceHeadersOptions {
-    headers?: string[];
+  headers?: readonly string[];
 }
 
-export const defaultHeaders = ['x-api-client', 'x-api-client-version'];
+export const defaultHeaders = ['x-api-client', 'x-api-client-version'] as const;
 
-const enforceHeaders: FastifyPluginCallback = async (fastify: FastifyInstance, {headers = defaultHeaders}: FastifyEnforceHeadersOptions = {}, next: (error?: FastifyError) => void): Promise<void> => {    
-    fastify.addHook('onRequest', async (req) => {
-      if (!headers.some((header: string) => !req.headers[header]?.toString())) {
-        return;
-      }
+const enforceHeaders: FastifyPluginAsync = async (
+  fastify: FastifyInstance,
+  { headers = defaultHeaders }: FastifyEnforceHeadersOptions = {},
+): Promise<void> => {
+  fastify.addHook('onRequest', async (req) => {
+    if (!headers.some((header: string) => !req.headers[header]?.toString())) {
+      return;
+    }
 
-      throw createError(400, 'Missing required headers in request');
-    });
-
-    next();
+    throw createError(400, 'Missing required headers in request');
+  });
 };
-export default fp(enforceHeaders, {name: '@osskit/fastify-enforce-headers', fastify: '4.x'});
+
+export default fp(enforceHeaders, { name: '@osskit/fastify-enforce-headers', fastify: '4.x' });
