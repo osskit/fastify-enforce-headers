9b0bcb3ab580a601412e1cbb569344c922d4c605
Daniel Witz <37773232+danielwitz@users.noreply.github.com>
2024-10-16 14:21:28 +0300

    support fastify 5 (#3)

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import { baseConfig, testConfig } from '@osskit/eslint-config';
+import typescriptEslint from 'typescript-eslint';
+
+export default typescriptEslint.config(
+  {
+    ignores: ['dist/**', '**/dist/**'],
+  },
+  {
+    ...baseConfig,
+    files: ['src/**/*.ts', 'tests/**/*.ts'],
+  },
+  {
+    ...testConfig,
+    files: ['**/*.spec.ts'],
+  },
+);
